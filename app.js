883b2445b70d17d95c7d125b24b2f14bddfb7888
Kevin Caballas <kevincaballas@gmail.com>
2022-07-28 01:05:23 +0800

    Connect database to application, update copy

--- app.js
+++ app.js
@@ -7,12 +7,20 @@ const app = new Koa();
 const router = new Router();
 
 router.get('/', ctx => {
-  ctx.body = 'Hello, world!';
+  ctx.body = 'Home is where the ❤️ is.';
 })
 
 app.use(Logger());
 
 app.use(router.routes()).use(router.allowedMethods());
+
+app.pool = new Pool({
+  user: 'budget-master-api',
+  host: 'localhost',
+  database: 'budget-master-api',
+  password: '',
+  port: 5432,
+});
 
 app.listen(3000, () => {
   console.log('Server running on port 3000.');
