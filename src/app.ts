4765a15d777358aeea9e224a8d3fe363d3cf3a27
Kevin Caballas <kevincaballas@gmail.com>
2022-07-28 03:40:40 +0800

    Refactor app file by removing non-initialization code

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,5 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-const Logger = require('koa-logger');
-const { Pool } = require('pg');
+import * as Koa from 'koa';
 
 const app = new Koa();
-const router = new Router();
 
-router.get('/', ctx => {
-  ctx.body = 'Home is where the ❤️ is.';
-})
-
-app.use(Logger());
-
-app.use(router.routes()).use(router.allowedMethods());
-
-app.pool = new Pool({
-  user: 'budget-master-api',
-  host: 'localhost',
-  database: 'budget-master-api',
-  password: '',
-  port: 5432,
-});
-
-app.listen(3000, () => {
-  console.log('Server running on port 3000.');
-})
+module.exports = app;
